09529f71a743d1ca433b2b38f0a30c05470a3bab
Khushal Abrol <khushalabrol14@gmail.com>
2022-04-12 00:53:45 +0530

    Added: add() Funtion to enter numbern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Scientific-Calculator-Using-DevOps-Tools';
+  value1: number = 0;
+  value2: number = 0;
+  performed: boolean = false;
+  expOprn: boolean = false;
+
+
+  add(x: number) {
+    if(this.performed){
+      this.value1 = x;
+      this.performed = false;
+      return;
+    }
+    if (this.expOprn) {
+      this.value1 = 0;
+    }
+    this.value1 = (this.value1 * 10) + x;
+  }
+
 }

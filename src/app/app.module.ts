7772cf8e76947ae097d4a109d641cfdaef537093
Khushal Abrol <khushalabrol14@gmail.com>
2022-04-28 09:55:15 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-import { environment } from '../environments/environment';
+// import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+// import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
-const environmentConfig: any = environment.logger;
+// const environmentConfig: any = environment.logger;
 
 @NgModule({
   declarations: [
@@ -14,11 +14,11 @@ const environmentConfig: any = environment.logger;
   imports: [
     BrowserModule,
     FormsModule,
-    LoggerModule.forRoot({
-      level: NgxLoggerLevel[environmentConfig.level],
-      serverLogLevel: NgxLoggerLevel[environmentConfig.serverLevel],
-      serverLoggingUrl: environmentConfig.serverUrl,
-    } as any)
+//     LoggerModule.forRoot({
+//       level: NgxLoggerLevel[environmentConfig.level],
+//       serverLogLevel: NgxLoggerLevel[environmentConfig.serverLevel],
+//       serverLoggingUrl: environmentConfig.serverUrl,
+//     } as any)
   ],
   providers: [],
   bootstrap: [AppComponent]
